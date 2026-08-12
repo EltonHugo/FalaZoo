2aa9cb3ae84550ef1e3d3c2d3364e617b9d7e201
Elton <eltonhugo@mail.com>
2026-08-12 16:59:31 -0400

    fix: ajusta funcoes

--- Challenge06/KidTranslate/TranslateService.swift
+++ Challenge06/KidTranslate/TranslateService.swift
@@ -11,15 +11,15 @@ import Translation
 
 @Observable
 class TranslateService {
-    var originalText = ""
-    var translatedText = ""
-    
     var inputLanguage = Locale.Language(identifier: "pt_BR")
     var outputLanguage = Locale.Language(identifier: "en_US")
     
     func translate(_ text: String) async -> String {
         let textoLimpo = text.trimmingCharacters(in: .whitespacesAndNewlines)
-        guard !textoLimpo.isEmpty else { return "Digite alguma coisa." }
+        guard !textoLimpo.isEmpty else {
+            print("")
+            return ""
+        }
         do {
             let session = TranslationSession(installedSource: inputLanguage, target: outputLanguage)
             let response = try await session.translate(textoLimpo)
